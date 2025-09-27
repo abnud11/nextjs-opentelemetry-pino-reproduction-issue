402c2a6d1b3683d4abcbeddd54e974f545af7978
abnud11 <ahmad13932013@gmail.com>
2025-09-27 13:37:40 +0300

    fix: show that direct logger call works

--- src/lib/actions/index.ts
+++ src/lib/actions/index.ts
@@ -1,9 +1,13 @@
 "use server";
 
 import { getLogger } from "../logger";
+import { logs } from "@opentelemetry/api-logs";
 
 export async function action(name: string) {
   const logger = await getLogger();
   logger.info(`Action called: ${name}`);
+  logs.getLogger("default").emit({
+    body: `this will show on otel collector ${name}`,
+  });
   return `Action ${name} completed`;
 }
